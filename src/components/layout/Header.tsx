ea36db819447c324478661faa32d91ddf3cdf8d6
MaxLiebsch <max@managbl.ai>
2025-08-29 14:30:51 +0200

    Adds SPA route handling and logo navigation link
    Adds _redirects file for SPA routing on Netlify that routes all paths to index.html
    
    Improves header by wrapping the logo in a link to the main website, enhancing navigation and user experience

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
       <nav className="bg-primary">
         <div className="container mx-auto h-24 max-w-7xl px-4 py-8 text-white xl:px-0">
           <div className="flex items-center justify-between">
-            <img src="/site-logo-white.svg" alt="ShelterBox" />
+            <a href="https://www.shelterbox.de/" target="_self" rel="noopener noreferrer">
+              <img src="/site-logo-white.svg" alt="ShelterBox" />
+            </a>
             <div className="flex items-center gap-4">
               <img src="/lock.svg" alt="Lock" className="h-6 w-6" />
               <h1 className="text-normal md:text-xl">Sicheres spenden</h1>
